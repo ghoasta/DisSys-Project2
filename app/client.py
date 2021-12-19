48037707eb7853f4c05c3a4e3c047506c8fcc05e
Andrzej Sokolowski <ghoasta18@gmail.com>
2021-12-19 10:11:05 +0000

    Final changes

--- app/client.py
+++ app/client.py
@@ -1,34 +1,29 @@
 import socket
+import sys
 
-# using socket to connect with server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# using the local server and the port 9856 on which the
-# server will connect.
 print("HR System 1.0")
-client.connect(('127.0.0.1', 9857))
+client.connect(('127.0.0.1', 9856))
 while True:
-
+    print("if you want to exit type X")
     empno = str(input("Enter Employee ID : "))
     empno = empno.strip().upper()
     client.send(empno.encode())
     status = client.recv(4096)
-    #print("Status : " + status.decode())
+
+    if empno == 'X':
+        sys.exit()
 
     if status.decode() == "VALID":
         command1 = str(input("Salary (S) or Annual Leave (L) Query? >> "))
         command1 = command1.strip().upper()
         client.send(command1.encode())
-        #print("Command1 == ",command1)
-
         comm1_reply = client.recv(4096)
         comm1_reply = comm1_reply.decode()
-        print(comm1_reply)
         if comm1_reply == 'S':
             command2 = str(input("Current salary (C) or total salary (T) for year? "))
             command2 = command2.strip().upper()
             client.send(command2.encode())
-            print("Command2 == ", command2)
-
             if command2 == 'C':
                 result = client.recv(4096)
                 result = result.decode()
@@ -39,11 +34,12 @@ while True:
                 result = client.recv(4096)
                 result = result.decode()
                 print(result)
+            else:
+                print("Invalid command")
 
-        if comm1_reply == 'L':
+        elif comm1_reply == 'L':
             command4 = str(input("Current Entitlement (C) or Leave taken for year (Y)? "))
             command4 = command4.strip().upper()
-            print("Command4 == ", command4)
             client.send(command4.encode())
             if command4 == 'C':
                 result = client.recv(4096)
@@ -55,7 +51,10 @@ while True:
                 result = client.recv(4096)
                 result = result.decode()
                 print(result)
-
+            else:
+                print("Invalid command")
+        else:
+            print("Invalid command")
 
     else:
         print("Sorry... I don’t recognise that employee id")
--- app/server.py
+++ app/server.py
@@ -1,92 +1,106 @@
+import json
 import os
 import socket
 from dataread import DataRead
+import pika
+from datetime import datetime
 
 # receiving message from client
 import threading
 
+
+def to_audit_queue(message):
+    # Send message to RabbitMQ and the close connection.
+    try:
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters("localhost")
+        )
+        channel = connection.channel()
+        channel.queue_declare(queue="audit-rabbit") #logging
+        channel.basic_publish(exchange="", routing_key="audit-rabbit", body=json.dumps(message))
+        connection.close()
+    except pika.exceptions.AMQPConnectionError:
+        print("Could not post to RabbitMQ")
+
+
 def newClient(con):
+    now = datetime.now()
     print("Connection from : ", adr)
     while True:
         empno = con.recv(4096)
         print("Employee id1 : " + empno.decode())
         exist = new_single.check_user_exist(empno.decode())
-        print(exist)
         if exist:
             con.send("VALID".encode())
             command1 = con.recv(4096)
             command1 = command1.decode()
-            print("Command1 : " + command1)
-
             if command1 == 'S':
                 con.send('S'.encode())
                 command2 = con.recv(4096)
                 command2 = command2.decode()
-                print("Command2 : " + command2)
                 if command2 == 'C':
+                    print("From: ",adr," Commands: ",command1,"->",command2)
                     name = new_single.get_name(empno.decode())
-                    print("NAME >> ", name)
                     current_salary = new_single.get_current_salary(empno.decode())
-                    print("CURRENT SALARY >> ", current_salary)
-                    mes = "Employee: "+name+ "\nCurrent basic salary: "+str(current_salary)
-                    print(mes)
-                    con.send(mes.encode())
-                elif command2 == 'T':
-                    #con.send('T'.encode())
-                    #print("years")
-                    command3 = con.recv(4096)
-                    command3 = command3.decode()
-                    #print(command3)
-                    basic, overtime = new_single.get_salary_year(empno.decode(),command3)
-                    #print(basic, overtime)
-                    mes = "Employee: "+new_single.get_name(empno.decode())+ "\nTotal Salary for year: "+command3+": " \
-                         "Basic pay: "+str(basic)+"; Overtime: "+str(overtime)
-                    #print(mes)
+                    mes = "Employee: " + name + "\nCurrent basic salary: " + str(current_salary)
+                    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+                    message = [adr, dt_string, empno.decode(), command1, command2]
+                    to_audit_queue(message)
                     con.send(mes.encode())
 
+                elif command2 == 'T':
+                    print("From: ", adr, " Commands: ", command1, "->", command2)
+                    command3 = con.recv(4096)
+                    command3 = command3.decode()
+                    basic, overtime = new_single.get_salary_year(empno.decode(), command3)
+                    mes = "Employee: " + new_single.get_name(
+                        empno.decode()) + "\nTotal Salary for year: " + command3 + ": " \
+                         "Basic pay: " + str(basic) + "; Overtime: " + str(overtime)
+                    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+                    message = [adr, dt_string, empno.decode(), command1, command2]
+                    to_audit_queue(message)
+                    con.send(mes.encode())
 
             if command1 == 'L':
                 con.send('L'.encode())
                 command4 = con.recv(4096)
                 command4 = command4.decode()
-                print("Command4 >> ",command4)
                 if command4 == 'C':
+                    print("From: ", adr, " Commands: ", command1, "->", command4)
                     current_leave = new_single.get_current_leave(empno.decode())
                     print(current_leave)
-                    mes = "Employee: "+new_single.get_name(empno.decode())+"\nCurrent annual leave entitlement: "+str(current_leave)
+                    mes = "Employee: " + new_single.get_name(
+                        empno.decode()) + "\nCurrent annual leave entitlement: " + str(current_leave)
                     print(mes)
+                    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+                    message = [adr, dt_string, empno.decode(), command1, command4]
+                    to_audit_queue(message)
                     con.send(mes.encode())
                 elif command4 == 'Y':
-                    print("YEAR")
+                    print("From: ", adr, " Commands: ", command1, "->", command4)
                     command5 = con.recv(4096)
                     command5 = command5.decode()
-                    leave = new_single.get_year_annual(empno.decode(),command5)
-                    print("LEAVE By year:",command5," >> ",leave)
-                    mes = "Employee: "+new_single.get_name(empno.decode())+"\nLeave taken in "+command5+": "+str(leave)
+                    leave = new_single.get_year_annual(empno.decode(), command5)
+                    print("LEAVE By year:", command5, " >> ", leave)
+                    mes = "Employee: " + new_single.get_name(
+                        empno.decode()) + "\nLeave taken in " + command5 + ": " + str(leave)
+                    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+                    message = [adr, dt_string, empno.decode(), command1, command4]
+                    to_audit_queue(message)
                     con.send(mes.encode())
-
-
-
         else:
             con.send("INVALID".encode())
-
-
-
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("Server started")
 print("Waiting for client request..")
-# binding port with ip and socket
-serv.bind(('127.0.0.1', 9857))
+serv.bind(('127.0.0.1', 9856))
 serv.listen(5)
 new_single = DataRead()
 
 counter = 0
-while 1:
-    #print("Waiting for client")
-    # waiting for client request
+while True:
     conn, adr = serv.accept()
-    ounter = counter + 1
+    counter = counter + 1
     t1 = threading.Thread(target=newClient, args=(conn,))
     t1.start()
-
